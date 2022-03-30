bb74b93b0fa0c232704e3597cdf3d2338a304a16
esarnb <esarnb@gmail.com>
2022-03-30 16:30:30 +0000

    connection in config, remove all old mongo code

--- Scripts/updateGitDB.ts
+++ Scripts/updateGitDB.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { gitRepos } from "../Types/gitTypes";
 dotenv.config();
 // const au = "token " + process.env.gtoken;
-const { host, user, pass, db, dialect } = process.env;
 
 export function updateGitDB(allRepos: gitRepos[]) {
     // post all repos to db with unique id
--- Sequelize/Models/GithubModel.ts
+++ Sequelize/Models/GithubModel.ts
@@ -1,42 +1,45 @@
-import mongoose, { Schema } from "mongoose"; 
-import config from "../config";
-import "colors";
 
-let { user, pwd, db } = config.mongoGithub;
-let conn = mongoose.createConnection(`mongodb://${ user }:${ pwd }@localhost/${ db }`, 
-{ useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true })
 
-conn.on('connected', () => {
-  console.log('Connected to githubDB'.blue);
-});
 
-conn.on('connecting', () => {
-  console.log('Connecting to githubDB'.yellow);
-});
+// import mongoose, { Schema } from "mongoose"; 
+// import config from "../config";
+// import "colors";
 
-conn.on('disconnected', () => {
-  console.log('Disconnected from githubDB'.red);
-});
+// let { user, pwd, db } = config.mongoGithub;
+// let conn = mongoose.createConnection(`mongodb://${ user }:${ pwd }@localhost/${ db }`, 
+// { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true })
 
-const GithubSchema = new Schema({
-  title: String,
-  desc: String,
-  live: String,
-  hasPage: Boolean,
-  lastRepoUpdate: Date,
-  lastServerUpdate: Date,
-  img: {
-      link: String,
-      name: String,
-  },
-  b1: {
-    link: String,
-    name: String,
-  },
-  b2: {
-    link: String,
-    name: String,
-  },
-});
+// conn.on('connected', () => {
+//   console.log('Connected to githubDB'.blue);
+// });
 
-export = conn.model("repositories", GithubSchema);
+// conn.on('connecting', () => {
+//   console.log('Connecting to githubDB'.yellow);
+// });
+
+// conn.on('disconnected', () => {
+//   console.log('Disconnected from githubDB'.red);
+// });
+
+// const GithubSchema = new Schema({
+//   title: String,
+//   desc: String,
+//   live: String,
+//   hasPage: Boolean,
+//   lastRepoUpdate: Date,
+//   lastServerUpdate: Date,
+//   img: {
+//       link: String,
+//       name: String,
+//   },
+//   b1: {
+//     link: String,
+//     name: String,
+//   },
+//   b2: {
+//     link: String,
+//     name: String,
+//   },
+// });
+
+// export = conn.model("repositories", GithubSchema);
