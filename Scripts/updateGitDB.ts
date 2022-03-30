9abccd293c6b419307b5c5f587c7725272b48858
esarnb <esarnb@gmail.com>
2022-03-30 16:30:49 +0000

    remove dotenv db connection

--- Scripts/updateGitDB.ts
+++ Scripts/updateGitDB.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
-import dotenv from "dotenv";
 import { gitRepos } from "../Types/gitTypes";
-dotenv.config();
-// const au = "token " + process.env.gtoken;
 
 export function updateGitDB(allRepos: gitRepos[]) {
     // post all repos to db with unique id
