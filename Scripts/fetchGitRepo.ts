30318df04cd3343e4136e78f23afbc57cf1cfa20
esarnb <esarnb@gmail.com>
2022-04-01 17:35:31 +0000

    removed colors package

--- Scripts/fetchGitRepo.ts
+++ Scripts/fetchGitRepo.ts
@@ -7,7 +7,7 @@ const au = "token " + process.env.gtoken;
 
 export function fetchGitRepo() {
 
-    let limit = 5000, remaining = null, reset = null, updated = null;
+    let limit:Number = 5000, remaining: Number = limit, reset: number | Date = new Date(), updated: string = "";
 
     const fetchData = async () => {
         if (remaining && remaining < 2) clearInterval(githubInterval);
@@ -24,12 +24,12 @@ export function fetchGitRepo() {
             updated = headers["etag"]; //etag id
             limit = +headers["x-ratelimit-limit"];
             remaining = +headers["x-ratelimit-remaining"];
-            reset = headers["x-ratelimit-reset"];
-            console.log(limit, remaining, updated, new Date(reset * 1000).toLocaleString());
+            reset = +headers["x-ratelimit-reset"];
+            console.log(limit, remaining, updated, new Date(1000 * reset).toLocaleString());
                         
             let data = response.data;
 
-            let gitResult: gitRepos[] = data.map(record => {
+            let gitResult: gitRepos[] = data.map((record: any) => {
                 return {
                     name: record.name, 
                     repo: record.html_url, 
