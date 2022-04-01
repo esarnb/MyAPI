2549d746e42e9e54afbe771ee32699840b689527
esarnb <esarnb@gmail.com>
2022-04-01 17:35:18 +0000

    Removed pics references

--- Routers/Github.ts
+++ Routers/Github.ts
@@ -14,50 +14,50 @@ router.get('/repos', async (req, res) => {
   res.json({records: "db records"})
 });
 
-router.get('/langs', (req, res) => {
-  let limit = 5000, remaining = null, reset = null, updated = null;
+// router.get('/langs', (req, res) => {
+//   // let limit:Number = 5000, remaining: Number = limit, reset: Date = new Date(), updated: string = "";
 
-  const fetchData = async () => {
-    try {
-        let response = await axios.get("https://api.github.com/users/esarnb/repos?per_page=100", {
-            headers: {
-                Authorization: au,
-                "If-None-Match": updated
-            }
-        });
+//   // const fetchData = async () => {
+//   //   try {
+//   //       let response = await axios.get("https://api.github.com/users/esarnb/repos?per_page=100", {
+//   //           headers: {
+//   //               Authorization: au,
+//   //               "If-None-Match": updated
+//   //           }
+//   //       });
 
-        // // "languages_url": "https://api.github.com/repos/esarnb/Bamazon/languages",
-        // let languages = await axios.get("https://api.github.com/repos/esarnb/Bamazon/languages")
+//   //       // // "languages_url": "https://api.github.com/repos/esarnb/Bamazon/languages",
+//   //       // let languages = await axios.get("https://api.github.com/repos/esarnb/Bamazon/languages")
         
 
-        let headers = response.headers
-        updated = headers["etag"]; //etag id
-        limit = +headers["x-ratelimit-limit"];
-        remaining = +headers["x-ratelimit-remaining"];
-        reset = new Date(headers["x-ratelimit-reset"]);
-        console.log(limit, remaining, updated, reset);
-        console.log(limit, remaining);
+//   //       let headers = response.headers
+//   //       updated = headers["etag"]; //etag id
+//   //       limit = +headers["x-ratelimit-limit"];
+//   //       remaining = +headers["x-ratelimit-remaining"];
+//   //       reset = new Date(headers["x-ratelimit-reset"]);
+//   //       console.log(limit, remaining, updated, reset);
+//   //       console.log(limit, remaining);
         
-        // setup auto refresh
-    } catch (err: any) {
-        console.error(err.message); // 304 etag unchanged
+//   //       // setup auto refresh
+//   //   } catch (err: any) {
+//   //       console.error(err.message); // 304 etag unchanged
         
         
-        axios.get("https://api.github.com/rate_limit", {
-            headers: {
-                Accept: "application/vnd.github.v3+json",
-                Authorization: au,
-            }
-        }).then((res) => {
-            // let headers = res.headers;
-            let data = res.data;
-            console.error(data.resources.core)
-        })
+//   //       axios.get("https://api.github.com/rate_limit", {
+//   //           headers: {
+//   //               Accept: "application/vnd.github.v3+json",
+//   //               Authorization: au,
+//   //           }
+//   //       }).then((res) => {
+//   //           // let headers = res.headers;
+//   //           let data = res.data;
+//   //           console.error(data.resources.core)
+//   //       })
 
 
-    }
-  }
-})
+//   //   }
+//   // }
+// })
 
 router.post("/add", (req, res) => {
   res.send("Post /DB")
--- Routers/index.ts
+++ Routers/index.ts
@@ -1,32 +1,19 @@
 import { Router } from "express"; // Express Router for grouping different pages
 import cors from "cors";
-import "colors";
 
 //API Routers
 import GithubRouter from "./Github"; 
 import CRUD from "./CRUD"; 
 // import DiscordRouter from "../Routers/Discord"; 
-// import PicsRouter from "../Routers/Pics"; 
+// import DBRouter from "./DB"; 
 
 // const app = express();
 const router = Router();
 router.use(cors());
 
-
-// =  =  =  =  =  =  =  =  =  =  Main  =  =  =  =  =  =  =  =  =  = //
 router.use("/", CRUD); 
-
-// =  =  =  =  =  =  =  =  =  =  Github  =  =  =  =  =  =  =  =  =  = //
 router.use("/github", GithubRouter); 
-
-// // =  =  =  =  =  =  =  =  =  =  Database  =  =  =  =  =  =  =  =  =  = //
-// import DBRouter from "./DB"; 
 // router.use("/db", DBRouter); 
-
-// =  =  =  =  =  =  =  =  =  =  Discord  =  =  =  =  =  =  =  =  =  = //
 // app.use("/discord", DiscordRouter); 
-
-// =  =  =  =  =  =  =  =  =  =  Pics  =  =  =  =  =  =  =  =  =  = //
-// app.use("/pics", PicsRouter); 
 
 export default router;
