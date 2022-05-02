4571e88519c434d93c5d5b238be212d726529944
esarnb <esarnb@gmail.com>
2022-05-02 05:37:15 +0000

    remove cookies to fix cors

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express"; // Express API for serving data and files
 import sequelizeInit from "./Sequelize/init";
+// import cookieSession from 'cookie-session';
 
 // Subdomain serving files [Routers]
 import index from "./Routers";
@@ -25,6 +26,15 @@ app.use((req, res, next) => {
 });
 
 app.use(index);
+
+// let options = {
+//   maxAge: 24 * 60 * 60 * 1000 // 24 hours
+// }
+// app.use(cookieSession(options));
+// app.use(function (req, res, next) {
+//   if (req?.sessionOptions?.maxAge && req?.session?.maxAge)
+//   req.sessionOptions.maxAge = req.session.maxAge || req.sessionOptions.maxAge
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
